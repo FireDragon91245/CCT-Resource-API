0394f01bc55d059e58495d3a10f1518f384c96d1
FireDragon91245 <rickvoss2@gmx.de>
2024-09-18 19:51:04 +0200

    Stream Close Handling on CC:T PC shutdown
    (cherry picked from commit 97f66fa501e0f22b7ae1d92b694ec592bed261b1)

--- src/main/java/org/firedragon91245/cctresourceapi/cct/LuaPcmSigned16SampleStream.java
+++ src/main/java/org/firedragon91245/cctresourceapi/cct/LuaPcmSigned16SampleStream.java
@@ -5,6 +5,7 @@ import dan200.computercraft.api.lua.LuaException;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import java.io.IOException;
 
 public class LuaPcmSigned16SampleStream extends LuaByteStream {
 
@@ -52,6 +53,18 @@ public class LuaPcmSigned16SampleStream extends LuaByteStream {
 
     protected AudioInputStream getBaseAudioStream() {
         return useConversionStream ? convertedAudioStream : baseAudioStream;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if(!closed)
+        {
+            if (useConversionStream && convertedAudioStream != null) {
+                convertedAudioStream.close();
+            }
+            baseAudioStream.close();
+            closed = true;
+        }
     }
 
     @Override
