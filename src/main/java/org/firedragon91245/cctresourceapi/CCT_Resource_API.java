bcf3a15e9ff278f4b55140a26d306748e35fb76f
FireDragon91245 <rickvoss2@gmx.de>
2024-09-24 22:34:06 +0200

    Service Loading Improvements

--- src/main/java/org/firedragon91245/cctresourceapi/CCT_Resource_API.java
+++ src/main/java/org/firedragon91245/cctresourceapi/CCT_Resource_API.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mojang.math.Vector3f;
 import dan200.computercraft.api.ComputerCraftAPI;
+import javazoom.spi.vorbis.sampled.file.VorbisAudioFileReader;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,6 +15,8 @@ import org.firedragon91245.cctresourceapi.json.*;
 
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.spi.AudioFileReader;
+import java.util.ServiceLoader;
 
 @Mod("cct_resource_api")
 public class CCT_Resource_API {
@@ -36,5 +39,9 @@ public class CCT_Resource_API {
             LOGGER.info(type.getExtension());
         }
 
+        ServiceLoader<AudioFileReader> reader = ServiceLoader.load(AudioFileReader.class, VorbisAudioFileReader.class.getClassLoader());
+        for (AudioFileReader r : reader) {
+            LOGGER.info(r.getClass().getName());
+        }
     }
 }
